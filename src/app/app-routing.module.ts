d13d8fd12a2ea446f2d04bbf28e5cd5d43d6f572
andresbermeoq <andipandi467@gmail.com>
2021-07-27 23:02:42 -0500

    Add Structure of The Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+
+const routes: Routes = [
+  { path: 'Registrarse', component: ClientesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
